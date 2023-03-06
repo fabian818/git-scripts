a6807a86577fbbeab80ede858e65b400416e3f36
Fabian Peña <fabian@LPT-WLD91M7HG0.local>
2023-03-06 16:12:26 -0500

    ADD: install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+export PATH=$PATH:~/opt/bin
+
+#!/bin/bash
+
+# Define the filename
+filename='~/.zshrc'
+current_path=$(pwd)
+
+# Append the text by using '>>' symbol
+echo "export PATH=$PATH:$current_path" >> ~/.zshrc
